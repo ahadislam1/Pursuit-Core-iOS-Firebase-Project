cbb00f1da17ac5fea706e7b0924854d6bc707563
Ahad Islam <ahad.islam624@gmail.com>
2020-04-30 11:32:00 -0400

    added unit testing

--- instagramproj/instagramprojTests/instagramprojTests.swift
+++ instagramproj/instagramprojTests/instagramprojTests.swift
@@ -8,17 +8,56 @@
 
 import XCTest
 import FirebaseAuth
+import Combine
 @testable import instagramproj
 
 class instagramprojTests: XCTestCase {
+    
+    var subscriptions = Set<AnyCancellable>()
 
     func testSignOut() {
+        
         do {
             try Auth.auth().signOut()
-            XCTAssert(0 == 0)
+            XCTAssert(Auth.auth().currentUser == nil)
         } catch {
             XCTFail("Failed: \(error.localizedDescription)")
         }
+    }
+    
+    func testLoadPhotos() {
+        let exp = XCTestExpectation()
+        FirestoreService.shared.loadPhotos()
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    XCTFail(error.localizedDescription)
+                }
+            }, receiveValue: { _ in
+                exp.fulfill()
+            })
+            .store(in: &subscriptions)
+        
+        wait(for: [exp], timeout: 2)
+    }
+    
+    func testImage() {
+        let imageView = UIImageView()
+        let exp = XCTestExpectation(description: "exp")
+        FirestoreService.shared.loadPhotos()
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    XCTFail(error.localizedDescription)
+                }}, receiveValue: { photos in
+                    guard let photo = photos.first, let url = URL(string: photo.imageURL) else {
+                        XCTFail("failed to get object")
+                        return
+                    }
+                    imageView.setImage(url: url, in: &self.subscriptions)
+                    exp.fulfill()
+            })
+            .store(in: &subscriptions)
+        
+        wait(for: [exp], timeout: 3)
     }
 
 }
